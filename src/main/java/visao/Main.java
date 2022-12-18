6b60794427e41f28e1e798548c37fb3306e4e8ee
Ana Camilly <camillyguedes2@gmail.com>
2022-12-18 03:46:42 -0300

    main com menu principal

--- src/main/java/visao/Main.java
+++ src/main/java/visao/Main.java
@@ -1,8 +1,49 @@
 package visao;
 
+import persistencia.CriancaDao;
+import persistencia.CuidadorDao;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static CriancaDao crianDao= new CriancaDao();
+    static CuidadorDao cuidadorDao= new CuidadorDao();
 
+    public static void main(String[] args) {
+        int digito;
+        Scanner teclado = new Scanner(System.in);
+
+        do {
+            System.out.println("\n");
+            System.out.print("|--------------------------------------------------------|\n");
+            System.out.print("|                     SISTEMA DE CRECHE                  |\n");
+            System.out.print("|--------------------------------------------------------|\n");
+            System.out.print("|                       MENU PRINCIPAL                   |\n"
+                    + "|--------------------------------------------------------|\n"
+                    + "| ESCOLHA UMA OPÇÃO ABAIXO:                              |\n"
+                    + "|--------------------------------------------------------|\n"
+                    + "| 1 - CRIANÇA                                            |\n"
+                    + "| 2 - CUIDADOR                                           |\n"
+                    + "| 3 - TURMA                                              |\n"
+                    + "| 4 - EXTRA-CURRICULAR                                   |\n"
+                    + "| 5 - SAIR DO SISTEMA                                    |\n"
+                    + "|--------------------------------------------------------|\n");
+
+            digito = teclado.nextInt();
+            switch(digito) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+        }while(digito != 5);
     }
 }
